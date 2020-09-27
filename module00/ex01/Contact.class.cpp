2f8c5a7ef0bc1d60bb43840d5c1d35781fe13ef1
Haroutioun Achtchian <haachtch@f1r6s3.codam.nl>
2020-09-27 14:34:48 +0200

    non printing first character of contact detail error fixed

--- module00/ex01/Contact.class.cpp
+++ module00/ex01/Contact.class.cpp
@@ -8,37 +8,26 @@ void	Contact::field(void)
 	std::cout << "\n---------ADD A CONTACT---------\n" << std::endl;
 
 	std::cout << "Enter fist name : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->firstName);
 	std::cout << "Enter last name : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin,this->lastName);
 	std::cout << "Enter nickname : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->nickname);
 	std::cout << "Enter login : " << std::endl;
-	std::cin.ignore();	
 	std::getline(std::cin, this->login);
 	std::cout << "Enter postal address : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->postalAddress);
 	std::cout << "Enter email address : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->emailAddress);
 	std::cout << "Enter phone number : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->phoneNumber);
 	std::cout << "Enter birthday date : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->birthdayDate);
 	std::cout << "Enter favorite meal : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->favoriteMeal);
 	std::cout << "Enter underwear color : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->underwearColor);
 	std::cout << "Enter darkest secret : " << std::endl;
-	std::cin.ignore();
 	std::getline(std::cin, this->darkestSecret);
 	std::cout << "\n---------------------------------\n" << std::endl;
 
