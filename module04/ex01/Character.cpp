b48cbcf2e0511b55ea279096c56f45d4838a109d
Haroutioun Achtchian <haroutioun24@gmail.com>
2020-10-16 14:13:28 +0200

    module04/ex01/ Character class update

---
+++ module04/ex01/Character.cpp
@@ -0,0 +1,47 @@
+#include "Character.hpp"
+
+Character::Character(void)
+{
+
+}
+
+Character::Character(std::string const &name)
+{
+
+}
+
+Character::Character(const Character &c)
+{
+
+
+}
+
+Character::~Character()
+{
+
+}
+
+Character& operator = (const Character &c)
+{
+
+}
+
+void Character::recoverAP(void)
+{
+
+}
+
+void Character::equip(AWeapon *w)
+{
+
+}
+
+void Character::attack(Enemy *e)
+{
+
+}
+
+std::string const getName(void) const 
+{
+
+}
--- module04/ex01/Character.hpp
+++ module04/ex01/Character.hpp
@@ -7,19 +7,24 @@
 class Character {
 
         private:
-			std::string _name;
-			unsigned int _aP;
-			AWeapon::AWeapon *aw;
+				std::string _name;
+				unsigned int _aP;
+				AWeapon::AWeapon *_weapon;
 
         public:
-                Character(std::string const & name);
+                Character(void);
+				Character(std::string const & name);
+				Character(const Character &c);
+				~Character(void);
 
-				[...]
-				~Character();
-				void recoverAP();
+				Character& operator = (const Character &c);
+
+				void recoverAP(void);
 				void equip(AWeapon*);
 				void attack(Enemy*);
-				std::string [...] getName() const;
+				std::string const getName(void) const;
 };
+
+std::ostream& operator << (std::ostream &output, const Character &c);
 
 #endif
