e302cd68d68aca9fb7966d1b8901119422fc4ea2
Haroutioun Achtchian <haroutioun24@gmail.com>
2020-10-16 14:16:26 +0200

    module04/ex01/ Character class update

--- module04/ex01/Character.cpp
+++ module04/ex01/Character.cpp
@@ -2,18 +2,21 @@
 
 Character::Character(void)
 {
-
+	this->_name = "Hello_wolrd";
+	this->_aP = 40;
+	this->_weapon = NULL;
 }
 
 Character::Character(std::string const &name)
 {
-
+	this->_name = name;
+	this->_aP = 40;
+	this->_weapon = NULL;
 }
 
 Character::Character(const Character &c)
 {
-
-
+	*this = c;
 }
 
 Character::~Character()
@@ -23,7 +26,9 @@ Character::~Character()
 
 Character& operator = (const Character &c)
 {
-
+	this->_name = c->_name;
+	this->_aP = c->_aP;
+	this_weapon = c->_weapon;
 }
 
 void Character::recoverAP(void)
