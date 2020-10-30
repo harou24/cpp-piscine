d6f8c9874d4302b7b86fd49e680ebef7afc2c587
Haroutioun <haroutioun24@gmail.com>
2020-10-30 19:20:55 +0100

    done

--- module07/ex00/whatever.cpp
+++ module07/ex00/whatever.cpp
@@ -10,12 +10,12 @@ void	swap(T& var1, T& var2) {
 
 template< typename T>
 const T&	min(const T& var1, const T& var2) {
-	return (var1 <= var2 ? var1 : var2);
+	return (var1 <= var2 ? var2 : var1);
 }
 
 template< typename T>
 const T&	max(const T& var1, const T& var2) {
-	return (var1 >= var2 ? var1 : var2);
+	return (var1 >= var2 ? var2 : var1);
 }
 
 
