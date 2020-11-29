8f679a3230103efe91e3a54bbb727d144ead492c
Haroutioun Achtchian <haroutioun24@gmail.com>
2020-11-29 18:37:06 +0100

    litle fixes

--- module08/ex02/mutantstack.hpp
+++ module08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: haachtch <haachtch@student.codam.nl>          +#+                    */
 /*                                                   +#+                      */
 /*   Created: 2020/11/29 18:04:18 by haachtch      #+#    #+#                 */
-/*   Updated: 2020/11/29 18:33:53 by haachtch      ########   odam.nl         */
+/*   Updated: 2020/11/29 18:36:53 by haachtch      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,22 @@
 # include <stack>
 # define container c 
 template <typename T>
-class MutantStack:
-	public std::stack< T, std::deque<T> > {
+class MutantStack : public std::stack< T, std::deque<T> > {
 
 	public:
 		typedef typename std::deque<T>::iterator iterator;
 		iterator	begin();
 		iterator	end();
-
 };
 
 template <typename T>
-typename MutantStack< T >::iterator		MutantStack< T >::begin()
+typename MutantStack< T >::iterator	MutantStack<T>::begin()
 {
 	return this->container.begin();
 }
 
 template <typename T>
-typename MutantStack<T>::iterator		MutantStack<T>::end()
+typename MutantStack<T>::iterator MutantStack<T>::end()
 {
 	return this->container.end();
 }
